18647d86475c28d1c15e9d970e9035179391495b
negod <silvajoaopedrossn@gmail.com>
2024-09-18 11:52:18 -0300

    Criando formulário

--- aula04/src/App.jsx
+++ aula04/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Exercicio1 from './components/Exercicio1'
 import Exercicio2 from './components/Exercicio2'
 import Campanha from './components/Campanha'
+import FormularioDeContato from './components/FormularioDeContato'
 
 const App = () => {
   return (
     <div>
       {/*<Exercicio1 */}
       {/*<Exercicio2 */}
-      <Campanha mes = "novembro" />
+      {/*<Campanha mes = "novembro" />*/}
+      <FormularioDeContato/>
     </div>
   )
 }
